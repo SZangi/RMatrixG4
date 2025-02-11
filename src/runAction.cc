4baaad00a1b919b6e3f9bda2f8a450c87928d2e1
Arthur Zangi <szangi@mit.edu>
2025-02-11 16:39:20 -0500

    Committing at start of adding PSD

--- src/runAction.cc
+++ src/runAction.cc
@@ -1,4 +1,5 @@
 #include "G4RunManager.hh"
+#include "G4AnalysisManager.hh"
 #include "runAction.hh"
 
 runAction::runAction()
@@ -11,6 +12,11 @@ void runAction::BeginOfRunAction(const G4Run *)
 {
     G4cout << "\n *********** Run Started *************"
     << G4endl;
+
+    auto analysisManager = G4AnalysisManager::Instance();
+    analysisManager->SetDefaultFileType("root");
+
+    analysisManager->CreateH2("psdHist","Photons Created vs. Photons in Tail",0,);
 }
 
 void runAction::EndOfRunAction(const G4Run *)
